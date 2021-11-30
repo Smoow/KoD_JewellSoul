16537f444e57a023e9501cce5486904ee23c3b6f
Pedro H. C <pedrohcm5@gmail.com>
2021-11-29 22:32:13 -0300

    [Update] Now 'Macro Joia' uses 'R' istead 'E' && Some changes on general.

--- KoD_JewellSoul.py
+++ KoD_JewellSoul.py
@@ -11,6 +11,7 @@ import win32com
 import win32gui
 import win32con
 import win32api
+import math
 
 from win32com import client
 from time import sleep
@@ -34,7 +35,7 @@ def show_credits():
 
 
 def choose_mode():
-    print('Modos: 1) Joia       2) Soul')
+    print('Modos: 1) Joia       2) Soul       3) Joia + Soul       4) Fada Lan (800x600)')
     mode = int(input('Digite o modo de operação do programa: '))
 
     return mode
@@ -69,7 +70,7 @@ def init_macro(mode):
     # Loop infinito para o macro
     if (mode == 1):
        print('\nModo escolhido: Renovação de Joia.')
-       print('Uso:\n\t- Deixe a joia de renovação no slot E (terceiro slot) da barra de atalho')
+       print('Uso:\n\t- Deixe a joia de renovação no slot R (quarto slot) da barra de atalho')
        time = int(
            input('\nDigite o tempo (em minutos) que você deseja renovar a joia: '))
 
@@ -77,13 +78,13 @@ def init_macro(mode):
        while(True):
             # [win32con.WM_CHAR] Isso é o que a SendMessage espere (suporta - e indica - tanto KeyDown como KeyUp)
             # [0x65] hexcode para 'E' (se precisar)
-            # [101] ascii code para 'e'
+            # [114] ascii code para 'R'
             # [0] Algo sobre retorno?
             # temp = win32api.SendMessage(hwndMain, win32con.WM_CHAR, 101, 0)
               print(
                 f'\n*** Enviando ao processo: &{process}   |   Renovando a cada {time} minutos! ***')
-              temp = win32api.SendMessage(process, win32con.WM_CHAR, 101, 0)
-              print(f"*** Item no slot '{chr(101)}' usado! ***\n")
+              temp = win32api.SendMessage(process, win32con.WM_CHAR, 114, 0)
+              print(f"*** Item no slot '{chr(114)}' usado! ***\n")
 
               # print(temp)
               sleep(60*time)
@@ -102,77 +103,85 @@ def init_macro(mode):
             shell.AppActivate("King of Destiny")
             print(
                     f'\n*** Enviando ao processo: &{process}   |   Renovando a cada 17 minutos! ***')
-            win32api.SendMessage(process, win32con.WM_CHAR, 0x31, 0)
+            win32api.SendMessage(process, win32con.WM_CHAR, 0x32, 0)
 
             # Para obter altura e largura da janela
+            shell.AppActivate("King of Destiny")
             rect = win32gui.GetWindowRect(process)
-            x = rect[0]+400
-            y = rect[1]+400
+            x1 = rect[0] 
+            y1 = rect[1] 
+            x2 = rect[2]
+            y2 = rect[3]
+
+            x_click = x1 + math.floor((x2-x1)/2) 
+            y_click = y1+ math.floor((y2-y1)/5) 
+
+            print(x1, y1, x2, y2)
 
             # Colocar o cursor na posição da janela
-            win32api.SetCursorPos((x, y))
+            win32api.SetCursorPos((x_click, y_click))
 
             # Click direito
-            win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x, y, 0, 0)
-            sleep(0.05)
-            win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x, y, 0, 0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, x1, y1, 0, 0)
+            sleep(0.1)
+            win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, x1, y1, 0, 0)
 
             # Para previnir a barra ficar travada, após a tentativa de usar a soul
             # é enviado um comando para setar a última skill da barra.
-            sleep(0.25)
+            sleep(1)
             win32api.SendMessage(process, win32con.WM_CHAR, 0x30, 0)
 
             sleep(60*17)
 
-    # if (mode == 3):
-    #    print('Em construção.')
-    #    return
+    if (mode == 3):
+       print('Em construção.')
+       return
 
-    # if (mode == 4):
-    #     print('\nModo escolhido: Macro de Fada para Lan.')
+    if (mode == 4):
+        print('\nModo escolhido: Macro de Fada para Lan.')
 
-    #     process = attach_process()
+        process = attach_process()
 
-    #     shell = win32com.client.Dispatch("WScript.Shell")
-    #     shell.AppActivate("King of Destiny")
-    #     print(
-    #             f'\n*** Enviando ao processo: &{process}   |   Macro de Fada para Lan! ***')
+        shell = win32com.client.Dispatch("WScript.Shell")
+        shell.AppActivate("King of Destiny")
+        print(
+                f'\n*** Enviando ao processo: &{process}   |   Macro de Fada para Lan! ***')
 
-    #     while (True):
-    #         # Para obter altura e largura da janela
-    #         rect = win32gui.GetWindowRect(process)
-    #         x = rect[0]+730
-    #         y = rect[1]+310
+        while (True):
+            # Para obter altura e largura da janela
+            rect = win32gui.GetWindowRect(process)
+            x = rect[0]+730
+            y = rect[1]+310
 
-    #         # Colocar o cursor na posição da janela
-    #         win32api.SetCursorPos((x, y))
+            # Colocar o cursor na posição da janela
+            win32api.SetCursorPos((x, y))
 
-    #         # Click Esquerdo no ultimo slot da primeira linha do inv
-    #         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    #         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+            # Click Esquerdo no ultimo slot da primeira linha do inv
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
-    #         # Click Esquerdo no slot da fada
-    #         y -= 160
-    #         win32api.SetCursorPos((x, y))
-    #         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    #         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-    #         sleep(2)
+            # Click Esquerdo no slot da fada
+            y -= 160
+            win32api.SetCursorPos((x, y))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+            sleep(2)
 
-    #         x = rect[0]+730
-    #         y = rect[1]+310
+            x = rect[0]+730
+            y = rect[1]+310
 
-    #         # Colocar o cursor na posição da janela
-    #         win32api.SetCursorPos((x, y))
+            # Colocar o cursor na posição da janela
+            win32api.SetCursorPos((x, y))
 
-    #         # Click Esquerdo no ultimo slot da primeira linha do inv
-    #         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    #         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+            # Click Esquerdo no ultimo slot da primeira linha do inv
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 
-    #         # Click Esquerdo no slot da fada
-    #         y -= 160
-    #         win32api.SetCursorPos((x, y))
-    #         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-    #         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
-    #         sleep(58)
+            # Click Esquerdo no slot da fada
+            y -= 160
+            win32api.SetCursorPos((x, y))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+            sleep(58)
 
 main()
